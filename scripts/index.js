f0b8ac7e10bbb147ee7e1cfe14a57bcaadc52b15
TagFri <115897998+TagFri@users.noreply.github.com>
2025-01-02 00:55:37 +0100

    Reset

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+import {masking} from './masking.js';
+import {eventListeners} from './inputHandling.js';
+import {initiateGraph} from './graph.js';
+
+masking();
+eventListeners();
+initiateGraph()
