2030c64161e365f25011c56167080ac0692f53bf
TagFri <115897998+TagFri@users.noreply.github.com>
2025-02-28 20:07:27 +0100

    custom select delete

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,8 @@
 import {masking} from "./masking.js";
 import {setValidationListener, validate} from './validation.js';
-import {customInput} from "./customInput.js";
 
 //Add custom masking to all inputs fields
 masking()
-//Add custom select input
-customInput()
 
 //Add listeners which validates all input fields
 setValidationListener()
--- scripts/masking.js
+++ scripts/masking.js
@@ -2,6 +2,10 @@ export {masking};
 
 //INPUT MASKING
 function masking() {
+    Inputmask("99/99").mask("birthDate")
+    Inputmask("99/99").mask("labDate")
+    Inputmask("99:99").mask("birthTime")
+    Inputmask("99:99").mask("labTime")
     Inputmask("99+9").mask("gestation")
     Inputmask("99[9] µmol/L").mask("bilirubinValue")
 };
